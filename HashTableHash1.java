344c56e47be461f022388a388cb4a4d6676f609e
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-05-22 14:01:42 -0300

    Create HashTableHash1.java

---
+++ HashTableHash1.java
@@ -0,0 +1,10 @@
+public class HashTableHash1 extends HashTable {
+    @Override
+    protected int hash(String key) {
+        int hash = 0;
+        for (char c : key.toCharArray()) {
+            hash += c;
+        }
+        return hash % size;
+    }
+}
