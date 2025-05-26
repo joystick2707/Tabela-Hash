b234e978afa1c430adc266055f54be5154aa8e83
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-05-26 13:58:11 -0300

    Update HashTable1.java

--- HashTable1.java
+++ HashTable1.java
@@ -1,10 +1,13 @@
+// HashTableHash1.java
 public class HashTable1 extends HashTable {
     @Override
     protected int hash(String key) {
         int hash = 0;
-        for (char c : key.toCharArray()) {
-            hash += c;
+        int prime = 31; // número primo comum usado em funções de hash
+        for (int i = 0; i < key.length(); i++) {
+            hash = prime * hash + key.charAt(i);
         }
-        return hash % size;
+        // Evita números negativos e garante que o hash esteja dentro dos limites da tabela
+        return Math.abs(hash) % size;
     }
 }
