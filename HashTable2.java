807b6d9e81159a6b645a07cc5be648bef24989a7
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-05-26 14:43:48 -0300

    Update HashTable2.java

--- HashTable2.java
+++ HashTable2.java
@@ -1,10 +1,10 @@
 public class HashTable2 extends HashTable {
     @Override
     protected int hash(String key) {
-        int hash = 0;
+        int hash = 5381; // Valor inicial usado no algoritmo DJB2
         for (int i = 0; i < key.length(); i++) {
-            hash = 31 * hash + key.charAt(i); // método de Horner
+            hash = ((hash << 5) + hash) ^ key.charAt(i); // hash * 33 ^ c
         }
-        return Math.abs(hash % size);
+        return Math.abs(hash) % size;
     }
 }
