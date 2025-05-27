5b99cffaa73b303c1982e6014d0a8ce496f80ddf
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-05-27 14:01:16 -0300

    Update FileReaderUtil.java

--- FileReaderUtil.java
+++ FileReaderUtil.java
@@ -1,13 +1,12 @@
 import java.io.*;
-import java.util.*;
 
 public class FileReaderUtil {
-    public static List<String> readNames(String filePath) throws IOException {
-        List<String> names = new ArrayList<>();
+    public static MyArrayList<String> readNames(String filePath) throws IOException {
+        MyArrayList<String> names = new MyArrayList<>(10); // capacidade inicial arbitrária
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
         String line;
         while ((line = reader.readLine()) != null) {
-            names.add(line.trim());
+            names.adicionar(line.trim());
         }
         reader.close();
         return names;
