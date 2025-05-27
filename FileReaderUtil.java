5c5e240dd3bd458a002ef21da8fe327f4eb6e051
vitin2212 <137333989+vitin2212@users.noreply.github.com>
2025-05-27 18:30:08 -0300

    Update FileReaderUtil.java

--- FileReaderUtil.java
+++ FileReaderUtil.java
@@ -1,6 +1,9 @@
 import java.io.*;
 
 public class FileReaderUtil {
+      // Define uma classe pública chamada FileReaderUtil. 
+    // Utilitária: normalmente usada para agrupar métodos auxiliares estáticos.
+
     public static MyArrayList<String> readNames(String filePath) throws IOException {
         MyArrayList<String> names = new MyArrayList<>(10); // capacidade inicial arbitrária
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
