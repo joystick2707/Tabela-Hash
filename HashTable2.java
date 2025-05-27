5befd91cf166f6f187abc4c4c7fed7a03c2f93a4
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-05-27 18:02:17 -0300

    codigo comentado

--- HashTable2.java
+++ HashTable2.java
@@ -1,10 +1,15 @@
 public class HashTable2 extends HashTable {
+
     @Override
     protected int hash(String key) {
         int hash = 5381; // Valor inicial usado no algoritmo DJB2
+
+        // Percorre cada caractere da chave
         for (int i = 0; i < key.length(); i++) {
-            hash = ((hash << 5) + hash) ^ key.charAt(i); // hash * 33 ^ c
+            // Atualiza o hash aplicando a operação: hash * 33 XOR caractere
+            hash = ((hash << 5) + hash) ^ key.charAt(i);
         }
+
         return Math.abs(hash) % size;
     }
 }
